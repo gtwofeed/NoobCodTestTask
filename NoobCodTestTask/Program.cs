5609ec2f44dd7b8f67562fa7ea44fbe2236f20f7
Fedia Hec <gtwofeed@gmail.com>
2022-11-26 19:13:51 +0300

    Добавьте файлы проекта.

---
+++ NoobCodTestTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+
+namespace NoobCodTestTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Читаем csv
+             * 
+             * Заполняем БД
+             * Подключаемся к эластика
+             * Обрабатываем поисковый запрос
+             */
+        }
+    }
+}
