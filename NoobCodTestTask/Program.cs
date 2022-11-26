1bfce1c1eadd4d03a49c8cea268cd5da23fd1ada
Fedia Hec <gtwofeed@gmail.com>
2022-11-26 21:46:42 +0300

    Чтения сым - ок

--- NoobCodTestTask/Program.cs
+++ NoobCodTestTask/Program.cs
@@ -1,18 +1,40 @@
 ﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System.Collections;
+using System.Globalization;
+using System.IO;
 
 namespace NoobCodTestTask
 {
+    [Delimiter(",")]
+    //[CultureInfo("")]
+    public class Post
+    {
+        [Name("text")]
+        public string Text { get; set; }
+        [Name("created_date")]
+        public string CreatedDate { get; set; }
+        [Name("rubrics")]
+        public string Rubrics { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*
-             * Читаем csv
-             * 
-             * Заполняем БД
-             * Подключаемся к эластика
-             * Обрабатываем поисковый запрос
-             */
+            // Читаем csv
+            // указываем путь к файлу csv
+            string pathCsvFile = "...\\posts\\posts.csv";
+            using (var reader = new StreamReader(pathCsvFile))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Post>();
+                Console.ReadKey();
+            }
+            
+            // Заполняем БД
+            // Подключаемся к эластика
+            // Обрабатываем поисковый запрос
         }
     }
 }
