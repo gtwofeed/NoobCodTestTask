30954c4a7b0a644d15836ec7fbc8608feceb038d
Fedia Hec <gtwofeed@gmail.com>
2022-12-03 22:42:04 +0300

    Доработал class Post (счетчик+, дата в формате даты, рубрики массивом строк) начал работу над подключением к базе

--- NoobCodTestTask/Program.cs
+++ NoobCodTestTask/Program.cs
@@ -4,6 +4,8 @@ using Npgsql;
 using System.Globalization;
 using System.Configuration;
 using System.Collections.Specialized;
+using System.Xml.Linq;
+using System.Collections.Generic;
 
 
 namespace NoobCodTestTask
@@ -11,20 +13,34 @@ namespace NoobCodTestTask
     [Delimiter(",")]
     public class Post
     {
-        static int Id { get; set; }
+        public static readonly string[] split = { "['", "']", "', '" };
+        string[] rubricsList;
+        static int countId;
         [Name("text")]
         public string Text { get; set; }
         [Name("created_date")]
-        public string CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
         [Name("rubrics")]
-        public string Rubrics { get; set; }
+        public string Rubrics
+        {
+            get { return string.Join(", ", rubricsList); }            
+            set { rubricsList = value.Split(split, StringSplitOptions.RemoveEmptyEntries); }
+        }
+        public int Id 
+        { 
+            get { return id; }
+        }
+        int id;
+        public Post() { id = ++countId; }
+    }
+    internal class ConectDB
+    {
+
     }
 
     internal class Program
     {
-        private static string connectionString;
-
-        static async Task Main(string[] args)
+        internal static async Task Main(string[] args)
         {
             // Читаем конфиг
             var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
@@ -37,17 +53,68 @@ namespace NoobCodTestTask
                 config.Save();
             }
             else Console.Write("Используется следующий файл данных: " + config.AppSettings.Settings["nameFile"].Value);
+            string nameFile = config.AppSettings.Settings["nameFile"].Value;
 
             // Читаем csv
             // указываем путь к файлу csv
-            string nameFile = config.AppSettings.Settings["nameFile"].Value;
+
             using (var reader = new StreamReader(nameFile))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 var records = csv.GetRecords<Post>();
-                Console.ReadKey();
+                var dataSource = DateBaseConect(config);
+                foreach (var post in records)
+                {
+                    DateBaseWrite(post, dataSource);
+                    Console.ReadKey();
+                }
+                
             }
 
+        }
+        internal static NpgsqlDataSource DateBaseConect(Configuration config)
+        {
+            // Конектимся к БД
+            string hostDB = "Host=" + config.AppSettings.Settings["hostDB"].Value;
+            string userNameDB = "Username=" + config.AppSettings.Settings["userNameDB"].Value;
+            string passwordDB = "Password=" + config.AppSettings.Settings["passwordDB"].Value;
+            string nameDB = "Database=" + config.AppSettings.Settings["nameDB"].Value;
+
+            string connectionString = hostDB + ";" + userNameDB + ";" + passwordDB + ";" + nameDB + ";";
+            var dataSource = NpgsqlDataSource.Create(connectionString);
+            return dataSource;
+
+        }
+        internal static async Task DateBaseWrite(Post post, NpgsqlDataSource dataSource)
+        {
+            // Insert some data
+            await using (var cmd = dataSource.CreateCommand("INSERT INTO message (text) VALUES (Hello    world)"))
+            {
+                //cmd.Parameters.AddWithValue("Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
+            Console.ReadKey();
+        }
+        internal static async Task RoughDraft()
+        {
+            // Правим конфиг
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["nameFile"].Value = Console.ReadLine();
+            config.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
+
+            Console.WriteLine(config.AppSettings.Settings["nameFile"].Value);
+            string nameFile = config.AppSettings.Settings["nameFile"].Value;
+            Console.ReadLine();
+
+            // Read all the keys from the config file
+            NameValueCollection sAll;
+            sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+                Console.WriteLine(s + " Value: " + sAll.Get(s));
+            Console.ReadLine();
             // Конектимся к БД
             string hostDB = "Host=";
             string userNameDB = "Username=";
@@ -57,16 +124,13 @@ namespace NoobCodTestTask
             string connectionString = hostDB + ";" + userNameDB + ";" + passwordDB + ";" + nameDB + ";";
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
 
-            Console.ReadLine();
-        }
-        static async Task RoughDraft()
-        {
-            var connectionString =
-                "Host=localhost;" +
-                "Username=postgres;" +
-                "Password=****;" +
-                "Database=post";
-            await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+            // Insert some data
+            await using (var cmd = dataSource.CreateCommand("INSERT INTO message (text) VALUES ($1)"))
+            {
+                cmd.Parameters.AddWithValue("Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
 
             // Retrieve all rows
             await using (var cmd = dataSource.CreateCommand("SELECT title FROM book"))
@@ -77,38 +141,26 @@ namespace NoobCodTestTask
                     Console.WriteLine(reader.GetString(0));
                 }
             }
+
             // Читаем csv
             // указываем путь к файлу csv
-            Console.ReadKey();
-            string pathCsvFile = "C:\\Users\\Ruslan\\source\\repos\\NoobCodTestTask\\posts\\posts.csv";
-            using (var reader = new StreamReader(pathCsvFile))
+            
+            using (var reader = new StreamReader(nameFile))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 var records = csv.GetRecords<Post>();
-                Console.ReadKey();
-            }
+                foreach (var record in records)
+                {
+                    Console.ReadKey();
+                }
 
-            // Заполняем БД
+            }
             // Подключаемся к эластика
+
+
             // Обрабатываем поисковый запрос
 
-            // Правим конфиг
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            config.AppSettings.Settings["nameFile"].Value = Console.ReadLine();
-            config.Save();
-
-            ConfigurationManager.RefreshSection("appSettings");
-
-            Console.WriteLine(config.AppSettings.Settings["nameFile"].Value);
-            Console.ReadLine();
-
-            // Read all the keys from the config file
-            NameValueCollection sAll;
-            sAll = ConfigurationManager.AppSettings;
-
-            foreach (string s in sAll.AllKeys)
-                Console.WriteLine(s + " Value: " + sAll.Get(s));
-            Console.ReadLine();
+            
         }
     }
 }
